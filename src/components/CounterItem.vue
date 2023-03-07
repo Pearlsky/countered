21ef4d521cb760e3cdd94b0093d1580d0e549b7a
pearlsky <pearlakpan115@gmail.com>
2023-03-07 21:03:17 +0100

    feat: write counterItem component ui & model; update code to use composables

---
+++ src/components/CounterItem.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+import useCounter from '../composables/useCounter'
+
+const editValue = ref('')
+const { getCount, increaseCount, decreaseCount, resetCount, setCount } = useCounter()
+</script>
+
+<template>
+  <section>
+    <span>{{ getCount }}</span>
+    <div>
+      <button @click="decreaseCount">-</button>
+      <button @click="resetCount">Reset</button>
+      <button @click="increaseCount">+</button>
+    </div>
+    <form action="">
+      <input v-model="editValue" type="text" />
+      <button
+        type="button"
+        @click="
+          () => {
+            setCount(editValue)
+            editValue = ''
+          }
+        "
+      >
+        Set
+      </button>
+    </form>
+  </section>
+</template>
