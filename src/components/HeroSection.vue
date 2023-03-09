c727919e1f2930f58ef067475a3f41277e9cdd7c
pearlsky <pearlakpan115@gmail.com>
2023-03-09 23:27:59 +0100

    feat: write ui for hero sec

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="max-w-[1240px] mx-auto my-20 md:my-24 text-center text-[#141B34]">
+    <div class="max-w-[600px] mx-auto">
+      <h1 class="text-5xl md:text-[4rem] lg:text-[5.5rem] leading-snug md:leading-normal">
+        Enhance your
+        <span
+          class="px-6 py-1 inline-block rounded-full bg-[url('../assets/radial-bg.svg')] bg-no-repeat bg-cover"
+          >workflow</span
+        >
+      </h1>
+    </div>
+    <div class="max-w-[800px] mx-auto">
+      <p class="my-10 md:my-14 text-[1.3rem] md:text-2xl lg:text-3xl md:leading-snug lg:leading-snug">
+        Welcome to Utils - a new and powerful platform dedicated to unlock your productivity by uniting all your utilities in one place.
+      </p>
+    </div>
+    <div class="max-w-[800px] mx-auto">
+      <button class="px-8 py-5 md:py-6 md:px-10 text-base md:text-[1.2rem] font-bold text-white bg-navybody border-0 rounded-full">
+        Get started
+      </button>
+    </div>
+    <div class="mt-16 md:mt-24 w-full">
+      <picture></picture>
+      <img class="max-w-full mx-auto" src="../assets/hero-mobile.svg" alt="" />
+    </div>
+  </section>
+</template>
