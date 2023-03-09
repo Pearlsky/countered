2a878b216e3bfd757f049d675d2f57aee18c8417
pearlsky <pearlakpan115@gmail.com>
2023-03-09 23:28:26 +0100

    feat: write navbar ui

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="flex items-center justify-between md:px-6">
+    <div><img src="../assets/logo.svg" alt="" /></div>
+    <div class="basis-[58%]">
+      <nav class="w-full">
+        <button class="md:hidden">X</button>
+        <div class="hidden md:flex md:justify-between md:items-center md:w-full">
+          <ul class="md:flex md:justify-between md:basis-[25%] text-base font-bold">
+            <li><a href="">Counter</a></li>
+            <li><a href="">Alarm</a></li>
+            <li><a href="">Stopwatch</a></li>
+          </ul>
+          <div>
+            <a
+              class="block px-4 py-1.5 md:py-4 md:px-6 text-base font-bold text-white bg-navybody border-0 rounded-full"
+              href=""
+              >Get started</a
+            >
+          </div>
+        </div>
+      </nav>
+    </div>
+  </section>
+</template>
