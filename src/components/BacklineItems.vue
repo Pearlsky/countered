6c4cf291897f9e421eef50dec87f181e804e6f04
pearlsky <pearlakpan115@gmail.com>
2023-03-10 16:21:38 +0100

    styles: write backline bg style component

---
+++ src/components/BacklineItems.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="relative">
+    <div
+      class="fixed block top-0 left-0 w-full h-full min-h-500px md:min-h-800px overflow-hidden opacity-[0.6] translate-x-0 translate-y-0 will-change-auto "
+    >
+      <div>
+        <div class="absolute block top-[-10vw] left-[-50vw] md:top-[-5vw] md:left-[-10vw] rotate-[-40deg] w-screen h-[100vw] md:w-[50vw] md:h-[40vw]">
+          <svg
+            class="absolute top-0 left-0 w-full h-full stroke-white stroke-[25] object-cover object-center"
+            viewBox="0 0 971 403"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              class="will-change-transform"
+              d="M23 23.1126C23 23.1126 71.0258 23.6768 159.5 23.1126C199.236 22.8592 145 23.1126 314.5 23.1126C484 23.1126 458.5 379.61 630 379.61C704.628 375.768 737.82 332.693 787.5 209.578C840.625 85.4082 864.334 52.8238 900.5 34.0977C925.646 21.2066 948.5 23.1126 948.5 23.1126"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+        </div>
+        <div class="absolute block top-[-20vw] right-[-40vw] md:top-[-15vw] md:right-[-5vw] rotate-[45deg] md:rotate-[35deg] w-screen h-[100vw] md:w-[50vw] md:h-[40vw]">
+          <svg
+            class="absolute top-0 left-0 w-full h-full stroke-white stroke-[25] object-cover object-center"
+            viewBox="0 0 971 403"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              class="will-change-transform"
+              d="M23 23.1126C23 23.1126 71.0258 23.6768 159.5 23.1126C199.236 22.8592 145 23.1126 314.5 23.1126C484 23.1126 458.5 379.61 630 379.61C704.628 375.768 737.82 332.693 787.5 209.578C840.625 85.4082 864.334 52.8238 900.5 34.0977C925.646 21.2066 948.5 23.1126 948.5 23.1126"
+            ></path>
+          </svg>
+        </div>
+        <div class="absolute block bottom-[-20vw] right-[-40vw] md:bottom-[-15vw] md:right-[-15vw] rotate-[125deg] w-screen h-[100vw] md:w-[50vw] md:h-[40vw]">
+          <svg
+            class="absolute top-0 left-0 w-full h-full stroke-white stroke-[25] object-cover object-center"
+            viewBox="0 0 971 403"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M23 23.1126C23 23.1126 71.0258 23.6768 159.5 23.1126C199.236 22.8592 145 23.1126 314.5 23.1126C484 23.1126 458.5 379.61 630 379.61C704.628 375.768 737.82 332.693 787.5 209.578C840.625 85.4082 864.334 52.8238 900.5 34.0977C925.646 21.2066 948.5 23.1126 948.5 23.1126"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+        </div>
+        <div class="absolute block bottom-[-25vw] left-[-40vw] md:bottom-[-20vw] md:left-[-15vw] rotate-[-120deg] md:rotate-[-125deg] w-screen h-[100vw] md:w-[50vw] md:h-[40vw]">
+          <svg
+            class="absolute top-0 left-0 w-full h-full stroke-white stroke-[25] object-cover object-center"
+            viewBox="0 0 971 403"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M23 23.1126C23 23.1126 71.0258 23.6768 159.5 23.1126C199.236 22.8592 145 23.1126 314.5 23.1126C484 23.1126 458.5 379.61 630 379.61C704.628 375.768 737.82 332.693 787.5 209.578C840.625 85.4082 864.334 52.8238 900.5 34.0977C925.646 21.2066 948.5 23.1126 948.5 23.1126"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
