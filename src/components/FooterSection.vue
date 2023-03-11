7004d1ada3c7da70aab41684381e6cebfe926fd7
pearlsky <pearlakpan115@gmail.com>
2023-03-11 21:58:51 +0100

    feat: add other nav links to footer & footer layout

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="max-w-[1240px] mx-auto mt-24 md:mt-28 text-white">
+    <div class="w-full h-full p-8 sm:p-14 md:p-16 lg:p-20 bg-navybody rounded-t-[3rem]">
+      <div class="max-w-[1000px] mx-auto">
+        <section class="md:flex">
+          <div class="flex flex-col items-center md:items-start md:basis-1/2">
+            <article><img src="../assets/logo-light.svg" alt="" /></article>
+            <ul>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+            </ul>
+          </div>
+          <div class="mt-8 md:mt-0 flex justify-center md:justify-end md:basis-1/2">
+            <div class="flex flex-col items-center md:items-start">
+              <h2 class="text-[1.3rem] md:text-3xl lg:text-5xl font-normal">We're here to ease</h2>
+              <p class="max-w-[250px] md:max-w-[450px] text-base  md:text-xl lg:text-2xl text-center md:text-left my-6">
+                If there's any more tools you need not covered in this web application, please get
+                in touch
+              </p>
+              <a
+                class="w-fit block px-4 py-2 md:py-4 md:px-8 text-base md:text-lg lg:text-xl font-bold text-navybody bg-[#CFC2FF] border-0 rounded-full cursor-pointer"
+                href=""
+                >Get in touch</a
+              >
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </section>
+</template>
