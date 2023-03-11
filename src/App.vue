6311bf05d897e49e9c1818c2d9096b6a193633e7
pearlsky <pearlakpan115@gmail.com>
2023-03-11 22:00:25 +0100

    refactor: implement router view and router links

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
-import NavBar from './components/NavBar.vue';
-import BacklineItems from './components/BacklineItems.vue';
-import HomeView from './views/HomeView.vue';
+import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue'
+import BacklineItems from './components/BacklineItems.vue'
 </script>
 
 <template>
   <div class="w-full h-full bg-[#ededee]">
-    <BacklineItems/>
-    <div class="h-full p-5">
-      <NavBar/>
-      <HomeView/>
+    <BacklineItems />
+    <div class="h-full px-5 pt-5">
+      <div class="relative">
+        <div class="">
+          <NavBar />
+          <RouterView/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
